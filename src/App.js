cce37692609fd4e430893ce682492bd8ebfa5787
Greta Ra <ggretute88@gmail.com>
2023-04-04 16:27:57 +0200

    Add tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './style/App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className="text-3xl font-bold underline text-green-700 text-center mt-3">
+      Hello world!
+      </h1>
     </div>
   );
 }
