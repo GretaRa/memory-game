66e7d3edc4d6d143f4a4cd02a4e37b0fa9a3247f
Greta Ra <ggretute88@gmail.com>
2023-04-06 16:01:59 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,51 +1,66 @@
+import { useState } from "react";
 import  Card  from "./components/Card.js";
+import Footer from "./components/Footer.js";
 
 function App() {
+
+  const [score, setScore] = useState(0);
+
+  const handleClick = () => {
+    setScore(score + 1)
+  }
+
   return (
-    <div className="App">
-      <p className="text-5xl text-center m-3">Plant memory game</p>
-      <div className="main grid grid-cols-1 md:grid-cols-[repeat(auto-fit,_minmax(20rem,_1fr))] gap-5 justify-items-center p-5 md:p-10">
-        <Card 
-          title = {'Peony'}
-          source = {"https://images.pexels.com/photos/2534524/pexels-photo-2534524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-        />
-        <Card 
-          title = {'Rose'}
-          source = {"https://images.pexels.com/photos/4899534/pexels-photo-4899534.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"}
-        />
-        <Card 
-          title = {'Tulip'}
-          source = {"https://images.pexels.com/photos/12374985/pexels-photo-12374985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-        />
-        <Card 
-          title = {'Sunflower'}
-          source = {"https://images.pexels.com/photos/54267/sunflower-blossom-bloom-flowers-54267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-        />
-        <Card 
-          title = {'Crocus'}
-          source = {"https://images.pexels.com/photos/348287/pexels-photo-348287.jpeg"}
-        />
-        <Card 
-          title = {'Dahlia'}
-          source = {"https://images.pexels.com/photos/2891092/pexels-photo-2891092.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-        />
-        <Card 
-          title = {'Daisy'}
-          source = {"https://images.pexels.com/photos/1196445/pexels-photo-1196445.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-        />
-        <Card 
-          title = {'Carnation'}
-          source = {"https://images.pexels.com/photos/8355483/pexels-photo-8355483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-        />
-        <Card 
-          title = {'Cosmos'}
-          source = {"https://images.pexels.com/photos/14613972/pexels-photo-14613972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-        />
-        <Card 
-          title = {'Aster'}
-          source = {"https://images.pexels.com/photos/85679/aster-tongolensis-michelmas-daisy-mauve-flower-85679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
-        />
+    <div className="App text-center flex flex-col h-screen">
+      <div className=" flex-grow">
+        <p className="text-5xl m-3">Plant memory game</p>
+        <span className="text-2xl">Your score: {score} Best score: 0</span>
+        <div className="main grid grid-cols-1 md:grid-cols-[repeat(auto-fit,_minmax(20rem,_1fr))] gap-5 justify-items-center p-5 md:p-10">
+          <Card 
+            onClick = {handleClick}
+            title = {'Peony'}
+            source = {"https://images.pexels.com/photos/2534524/pexels-photo-2534524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+          />
+          <Card 
+            title = {'Rose'}
+            source = {"https://images.pexels.com/photos/4899534/pexels-photo-4899534.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"}
+          />
+          <Card 
+            title = {'Tulip'}
+            source = {"https://images.pexels.com/photos/12374985/pexels-photo-12374985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+          />
+          <Card 
+            title = {'Sunflower'}
+            source = {"https://images.pexels.com/photos/54267/sunflower-blossom-bloom-flowers-54267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+          />
+          <Card 
+            title = {'Crocus'}
+            source = {"https://images.pexels.com/photos/348287/pexels-photo-348287.jpeg"}
+          />
+          <Card 
+            title = {'Dahlia'}
+            source = {"https://images.pexels.com/photos/2891092/pexels-photo-2891092.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+          />
+          <Card 
+            title = {'Daisy'}
+            source = {"https://images.pexels.com/photos/1196445/pexels-photo-1196445.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+          />
+          <Card 
+            title = {'Carnation'}
+            source = {"https://images.pexels.com/photos/8355483/pexels-photo-8355483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+          />
+          <Card 
+            title = {'Cosmos'}
+            source = {"https://images.pexels.com/photos/14613972/pexels-photo-14613972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+          />
+          <Card 
+            title = {'Aster'}
+            source = {"https://images.pexels.com/photos/85679/aster-tongolensis-michelmas-daisy-mauve-flower-85679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+          />
+          
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <div className="bg-[#EBBAB9] border-[#B89291] border-t-2 p-3">
+      Designed and coded by Greta Rakauskaite 2023
+    </div>
+  )
+}
+
+export default Footer
